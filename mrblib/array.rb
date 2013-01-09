ad98e3983f90edfe653982df51971ad821a41a46
skandhas <skandhas@163.com>
2013-01-09 17:48:24 +0800

    add comments

--- mrblib/array.rb
+++ mrblib/array.rb
@@ -1,4 +1,16 @@
 class Array
+
+  ##
+  # call-seq:
+  #    ary.reject! {|item| block }  -> ary or nil
+  #
+  # Equivalent to <code>Array#delete_if</code>, deleting elements from
+  # +self+ for which the block evaluates to true, but returns
+  # <code>nil</code> if no changes were made.
+  # The array is changed instantly every time the block is called and
+  # not after the iteration is over.
+  # See also <code>Enumerable#reject</code> and <code>Array#delete_if</code>.
+
   def reject!(&block)
     was = self.size
     each_index do |idx|
@@ -16,14 +28,47 @@ class Array
     end
   end
 
+  ##
+  # call-seq:
+  #    ary.delete_if {|item| block }  -> ary
+  #    ary.delete_if                  -> an_enumerator
+  #
+  # Deletes every element of +self+ for which <i>block</i> evaluates
+  # to true.
+  # The array is changed instantly every time the block is called and
+  # not after the iteration is over.
+  # See also <code>Array#reject!</code>
+  #
+  #    a = [ "a", "b", "c" ]
+  #    a.delete_if {|x| x >= "b" }   #=> ["a"]
+
   def delete_if(&block)
     self.reject!(&block) || self 
   end
 
+  ##
+  # call-seq:
+  #    ary.compact!    -> ary  or  nil
+  #
+  # Removes +nil+ elements from the array.
+  # Returns +nil+ if no changes were made, otherwise returns
+  # <i>ary</i>.
+  #
+  #    [ "a", nil, "b", nil, "c" ].compact! #=> [ "a", "b", "c" ]
+  #    [ "a", "b", "c" ].compact!           #=> nil
+
   def compact!
-    self.reject!{|e| e == nil }
+    self.reject!{|e| e.nil? }
   end
 
+  ##
+  #  call-seq:
+  #     ary.compact     -> new_ary
+  #
+  #  Returns a copy of +self+ with all +nil+ elements removed.
+  #
+  #     [ "a", nil, "b", nil, "c", nil ].compact
+  #                       #=> [ "a", "b", "c" ]
   def compact
     ary = self.dup
     ary.compact! || ary 
--- mrblib/enum.rb
+++ mrblib/enum.rb
@@ -1,10 +1,40 @@
 module Enumerable
+
+  ##
+  # call-seq:
+  #    enum.drop(n)               -> array
+  #
+  # Drops first n elements from <i>enum</i>, and returns rest elements
+  # in an array.
+  #
+  #    a = [1, 2, 3, 4, 5, 0]
+  #    a.drop(3)             #=> [4, 5, 0]
+
   def drop(n)
     n = n.to_int
     raise ArgumentError, "attempt to drop negative size" if n < 0
 
     self[n, self.size] 
   end
+
+  ##
+  # call-seq:
+  #   enum.each_cons(n) {...}   ->  nil
+  #
+  # Iterates the given block for each array of consecutive <n>
+  # elements.  If no block is given, returns an enumerator.
+  #
+  # e.g.:
+  #     (1..10).each_cons(3) {|a| p a}
+  #     # outputs below
+  #     [1, 2, 3]
+  #     [2, 3, 4]
+  #     [3, 4, 5]
+  #     [4, 5, 6]
+  #     [5, 6, 7]
+  #     [6, 7, 8]
+  #     [7, 8, 9]
+  #     [8, 9, 10]
 
   def each_cons(n)
     n = n.to_int 
@@ -17,6 +47,20 @@ module Enumerable
       yield ary.dup if ary.size == n
     end
   end
+
+  ##
+  # call-seq:
+  #   enum.each_slice(n) {...}  ->  nil
+  #
+  # Iterates the given block for each slice of <n> elements.
+  #
+  # e.g.:
+  #     (1..10).each_slice(3) {|a| p a}
+  #     # outputs below
+  #     [1, 2, 3]
+  #     [4, 5, 6]
+  #     [7, 8, 9]
+  #     [10]
 
   def each_slice(n)
     n = n.to_int 
@@ -33,9 +77,18 @@ module Enumerable
     yield ary unless ary.empty?
   end
 
+  ##
+  # call-seq:
+  #    enum.group_by {| obj | block }  -> a_hash
+  #
+  # Returns a hash, which keys are evaluated result from the
+  # block, and values are arrays of elements in <i>enum</i>
+  # corresponding to the key.
+  #
+  #    (1..6).group_by {|i| i%3}   #=> {0=>[3, 6], 1=>[1, 4], 2=>[2, 5]}
+
   def group_by
     h = {}
-    i = 0
     self.each do |e|
       key = yield(e)
       if h.key?(key)
