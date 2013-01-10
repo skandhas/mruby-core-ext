c5dd0084bdee424e1e5433ce4dd1f6eab9407e3e
skandhas <skandhas@163.com>
2013-01-10 11:12:04 +0800

    implement Hash#delete_if by Hash#reject!

--- mrblib/hash.rb
+++ mrblib/hash.rb
@@ -10,9 +10,7 @@ class Hash
   #    h = { "a" => 100, "b" => 200, "c" => 300 }
   #    h.delete_if {|key, value| key >= "b" }   #=> {"a"=>100}
 
-  def delete_if
-    self.each do |k, v|
-      self.delete(k) if yield(k, v) 
-    end   
+  def delete_if(&block)
+    self.reject!(&block) || self
   end  
 end
