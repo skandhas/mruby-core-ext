e30ca605bca0c632b319bc5c3bd6aec36ad8db78
skandhas <skandhas@163.com>
2013-01-10 13:48:36 +0800

    fix test for Hash#delete_if, Hash#keep_if

--- test/hash.rb
+++ test/hash.rb
@@ -1,5 +1,11 @@
 assert("Hash#delete_if") do
   h = { "a" => 100, "b" => 200, "c" => 300 }
   h.delete_if {|key, value| key >= "b" } == { "a" => 100 } and
-  h.delete_if {|key, value| key >= "e" } == h
+  h.delete_if {|key, value| key == "e" } == { "a" => 100 }
+end
+
+assert("Hash#keep_if") do
+  h = { "a" => 100, "b" => 200, "c" => 300 }
+  h.keep_if {|key, value| key < "b" } == { "a" => 100 } and
+  h.keep_if {|key, value| key == "d" } == {}
 end
