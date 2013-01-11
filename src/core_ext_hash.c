0f8559fb8a98ba97df5822999777dde0ec7fcf60
skandhas <skandhas@163.com>
2013-01-11 13:33:13 +0800

    add Hash::try_convert, Hash#assoc

--- src/core_ext_hash.c
+++ src/core_ext_hash.c
@@ -1,9 +1,25 @@
 #include <mruby.h>
 #include <mruby/value.h>
-#include <mruby/array.h>
+#include <mruby/hash.h>
+
+extern mrb_value mrb_hash_assoc(mrb_state *mrb, mrb_value hash);
+
+static mrb_value
+mrb_core_ex_hash_try_convert(mrb_state *mrb, mrb_value self)
+{
+  mrb_value hash;
+
+  mrb_get_args(mrb, "o", &hash);
+  return mrb_check_hash_type(mrb, hash);
+}
+
 
 void
 mrb_init_core_ex_hash(mrb_state * mrb)
 {
+  struct RClass *h;
 
+  h = mrb->hash_class;
+  mrb_define_class_method(mrb, h, "try_convert", mrb_core_ex_hash_try_convert, ARGS_REQ(1));
+  mrb_define_method(mrb, h, "assoc", mrb_hash_assoc, ARGS_REQ(1));
 }
--- test/hash.rb
+++ test/hash.rb
@@ -1,3 +1,21 @@
+assert("Hash::try_convert") do
+ Hash.try_convert({1=>2}) == {1=>2} and
+ Hash.try_convert("1=>2").nil?
+end
+
+assert("Hash#assoc") do
+  h = {"colors"  => ["red", "blue", "green"],
+       "letters" => ["a", "b", "c" ]}
+
+  h.assoc("letters") == ["letters", ["a", "b", "c"]] and
+  h.assoc("foo").nil?
+end
+
+#assert("Hash#rassoc") do
+#  a = {1=> "one", 2 => "two", 3 => "three", "ii" => "two"}
+#  a.rassoc("two") == [2, "two"] and  a.rassoc("four").nil?
+#end
+
 assert("Hash#delete_if") do
   h = { "a" => 100, "b" => 200, "c" => 300 }
   h.delete_if {|key, value| key >= "b" } == { "a" => 100 } and
