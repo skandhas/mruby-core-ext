40fb64496940d6d40f1fa9dbd76c19eaf43e904e
skandhas <skandhas@163.com>
2013-01-11 11:28:18 +0800

    add src-file:core_ext_hash.c

---
+++ src/core_ext_hash.c
@@ -0,0 +1,9 @@
+#include <mruby.h>
+#include <mruby/value.h>
+#include <mruby/array.h>
+
+void
+mrb_init_core_ex_hash(mrb_state * mrb)
+{
+
+}
--- src/mrb_core_ext.c
+++ src/mrb_core_ext.c
@@ -3,9 +3,11 @@
 #include <mruby/array.h>
 
 void mrb_init_core_ex_array(mrb_state * mrb);
+void mrb_init_core_ex_hash(mrb_state * mrb);
 
 void
 mrb_mruby_core_ext_gem_init(mrb_state * mrb)
 {
   mrb_init_core_ex_array(mrb);
+  mrb_init_core_ex_hash(mrb);
 }
