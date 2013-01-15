337ef2eb66cde12d7f547ae59c37ccb2d950787b
skandhas <skandhas@163.com>
2013-01-15 16:00:17 +0800

    add Symbol#capitalize

--- mrblib/symbol.rb
+++ mrblib/symbol.rb
@@ -9,4 +9,14 @@ class Symbol
     self.to_s.length
   end
   alias :size :length
+
+  ##
+  # call-seq:
+  #   sym.capitalize  -> symbol
+  #
+  # Same as <code>sym.to_s.capitalize.intern</code>.
+
+  def capitalize
+    self.to_s.capitalize.intern
+  end
 end
--- test/symbol.rb
+++ test/symbol.rb
@@ -1,3 +1,9 @@
 assert("Symbol#length") do
   :I.length  == 1 and :love.size == 4 and :mruby.length == 5
 end
+
+assert("Symbol#capitalize") do
+  :hello.capitalize == :Hello and :HELLO.capitalize == :Hello
+end
+
+
